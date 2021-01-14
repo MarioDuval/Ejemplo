cf9693c43b1b5176959e291efab61b2ebbc54482
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2021-01-14 16:57:41 +0100

    Initial commit

---
+++ src/Exemple.java
@@ -0,0 +1,21 @@
+/*  main
+*   @autor Mario Duval
+*   @version 1.0
+ */
+
+    public class Exemple{
+        public static void main(String[] args) {
+            int a=1, b=2;
+            addValues(a,b);
+        }
+
+        /*
+        * @param a integer
+        * @param b integer
+        * @return integer
+         */
+
+        public static int addValues(int a, int b){
+            return a+b;
+        }
+    }
